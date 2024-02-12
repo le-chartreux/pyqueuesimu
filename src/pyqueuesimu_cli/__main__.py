81e4682575994a43c486e5ba316c15992487ea1a
le-chartreux <le-chartreux-vert@protonmail.com>
2024-02-12 11:17:24 +0100

    feat: add main

--- src/pyqueuesimu_cli/__main__.py
+++ src/pyqueuesimu_cli/__main__.py
@@ -1,1 +1,12 @@
 """Command line interface executable of pyqueuesimu."""
+import typer
+from rich import print
+
+
+app = typer.Typer()
+
+
+@app.command()
+def pyqueuesimu_cli() -> None:
+    """Run the queue simulation."""
+    print("hello world!")
