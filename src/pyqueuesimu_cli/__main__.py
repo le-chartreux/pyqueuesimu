8565b0453fd697d4ea96e2244552f8592da92e1f
le-chartreux <le-chartreux-vert@protonmail.com>
2024-02-12 11:51:55 +0100

    feat: add parameters to main

--- src/pyqueuesimu_cli/__main__.py
+++ src/pyqueuesimu_cli/__main__.py
@@ -7,6 +7,18 @@ app = typer.Typer()
 
 
 @app.command()
-def pyqueuesimu_cli() -> None:
-    """Run the queue simulation."""
-    print("hello world!")
+def pyqueuesimu_cli(
+    constant_arrival_rate: float,
+    exponential_service_law_parameter: float,
+    observation_duration: float = 60
+) -> None:
+    """Run the queue simulation.
+
+    Args:
+        - constant_arrival_rate: inter-arrival time of clients.
+        - exponential_service_law_parameter: parameter of the exponential law that
+            the service time follows.
+        - observation_duration: how long the observation should last.
+    """
+    print(f"The inter-arrival time of clients is {constant_arrival_rate}.")
+    print(f"The service time follows an exponential law of parameter {exponential_service_law_parameter}.")
