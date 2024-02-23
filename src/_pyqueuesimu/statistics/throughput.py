70c3f61bf2c9a63aff3b5f5a957428f3e8b16dfc
le-chartreux <le-chartreux-vert@protonmail.com>
2024-02-23 22:28:04 +0100

    fix: reformat

--- src/_pyqueuesimu/statistics/throughput.py
+++ src/_pyqueuesimu/statistics/throughput.py
@@ -1,6 +1,10 @@
-def get_incoming_throughput(arrivals: list[float], observation_duration: float) -> float:
+def get_incoming_throughput(
+    arrivals: list[float], observation_duration: float
+) -> float:
     return len(arrivals) / observation_duration
 
 
-def get_outgoing_throughput(departures: list[float], observation_duration: float) -> float:
+def get_outgoing_throughput(
+    departures: list[float], observation_duration: float
+) -> float:
     return len(departures) / observation_duration
