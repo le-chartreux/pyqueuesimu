765cc9759a6abcd19620b8b4db1d8c98b87bccbe
le-chartreux <le-chartreux-vert@protonmail.com>
2024-02-23 23:55:39 +0100

    fix: add docstrings

--- src/_pyqueuesimu/statistics/throughput.py
+++ src/_pyqueuesimu/statistics/throughput.py
@@ -1,10 +1,12 @@
 def get_incoming_throughput(
     arrivals: list[float], observation_duration: float
 ) -> float:
+    """Compute the incoming throughput."""
     return len(arrivals) / observation_duration
 
 
 def get_outgoing_throughput(
     departures: list[float], observation_duration: float
 ) -> float:
+    """Compute the outgoing throughput."""
     return len(departures) / observation_duration
