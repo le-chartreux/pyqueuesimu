ea986a2bb3ba7e580164a8f115df472ba037bc3b
le-chartreux <le-chartreux-vert@protonmail.com>
2024-02-23 22:27:52 +0100

    feat: add stat outgoing throughput and test it

--- src/_pyqueuesimu/statistics/throughput.py
+++ src/_pyqueuesimu/statistics/throughput.py
@@ -1,2 +1,6 @@
 def get_incoming_throughput(arrivals: list[float], observation_duration: float) -> float:
     return len(arrivals) / observation_duration
+
+
+def get_outgoing_throughput(departures: list[float], observation_duration: float) -> float:
+    return len(departures) / observation_duration
--- tests/_pyqueuesimu/statistics/test_throughput.py
+++ tests/_pyqueuesimu/statistics/test_throughput.py
@@ -1,6 +1,6 @@
 import pytest
 
-from _pyqueuesimu.statistics.throughput import get_incoming_throughput
+from _pyqueuesimu.statistics.throughput import get_incoming_throughput, get_outgoing_throughput
 
 
 @pytest.mark.parametrize(
@@ -13,3 +13,15 @@ from _pyqueuesimu.statistics.throughput import get_incoming_throughput
 )
 def test_get_incoming_throughput(arrivals: list[float], observation_duration: float, expected_result: float) -> None:
     assert get_incoming_throughput(arrivals, observation_duration) == expected_result
+
+
+@pytest.mark.parametrize(
+    ("arrivals", "observation_duration", "expected_result"),
+    [
+        ([], 100, 0),
+        ([1, 2, 18], 30, 0.1),
+        ([0.4, 0.15, 1.54, 1.61, 1.89, 1.99], 2, 3)
+    ]
+)
+def test_get_outgoing_throughput(arrivals: list[float], observation_duration: float, expected_result: float) -> None:
+    assert get_outgoing_throughput(arrivals, observation_duration) == expected_result
