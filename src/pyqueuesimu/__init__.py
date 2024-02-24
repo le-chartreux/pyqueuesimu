35fbeb2334e06ac33ca64374c3909a7fc67ba146
le-chartreux <le-chartreux-vert@protonmail.com>
2024-02-24 22:31:31 +0100

    feat: expose get_departure_times_limited_buffer to public api

--- src/pyqueuesimu/__init__.py
+++ src/pyqueuesimu/__init__.py
@@ -1,7 +1,10 @@
 """Public package of pyqueuesimu."""
 
 from _pyqueuesimu.generation.arrival_times import get_arrival_times
-from _pyqueuesimu.generation.departure_times import get_departure_times
+from _pyqueuesimu.generation.departure_times import (
+    get_departure_times,
+    get_departure_times_limited_buffer,
+)
 from _pyqueuesimu.generation.inter_arrival_times import generate_inter_arrival_times
 from _pyqueuesimu.generation.service_times import generate_service_times
 from _pyqueuesimu.statistics.clients import (
@@ -26,6 +29,7 @@ __all__ = [
     "get_confidence_interval_95_percents",
     "get_arrival_times",
     "get_departure_times",
+    "get_departure_times_limited_buffer",
     "generate_inter_arrival_times",
     "generate_service_times",
     "get_waiting_times",
