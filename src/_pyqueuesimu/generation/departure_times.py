842b88f3b092ef7fe93e5b8091c139b82943f915
le-chartreux <le-chartreux-vert@protonmail.com>
2024-02-24 22:30:18 +0100

    feat: add & test get_departure_times_limited_buffer

--- src/_pyqueuesimu/generation/departure_times.py
+++ src/_pyqueuesimu/generation/departure_times.py
@@ -16,6 +16,31 @@ def get_departure_times(
     Raises:
         ValueError: When len(arrival_times) != len(service_times)
     """
+    # same that with a limited buffer that can fit everything
+    return get_departure_times_limited_buffer(arrival_times, service_times, len(arrival_times))
+
+
+def get_departure_times_limited_buffer(
+    arrival_times: list[float], service_times: list[float], buffer_size: int
+) -> list[float]:
+    """Get the ordinated list of time departures with a limited buffer size.
+
+    Args:
+        arrival_times: ordinated list where result[i] is the moment where client i
+            arrived.
+        service_times: ordinated list where service_times[i] is the number of time units
+            between the arrival of client i and its departure.
+        buffer_size: size of the buffer. If the number of clients is greater than the
+            buffer size + 1 (because of the client that is currently serviced), the
+            next clients to arrive will be lost.
+
+    Returns:
+        An ordinated list where result[i] is the moment where client i left the system.
+        If client i was lost, result[i] = -1
+
+    Raises:
+        ValueError: When len(arrival_times) != len(service_times)
+    """
     if len(arrival_times) != len(service_times):
         error_message = (
             "len(arrival_times) != len(service_times): "
@@ -25,8 +50,14 @@ def get_departure_times(
     departure_times = []
     previous_departure_time = 0.0
     for arrival_time, service_time in zip(arrival_times, service_times, strict=True):
-        beginning_of_computation_time = max(arrival_time, previous_departure_time)
-        departure_time = beginning_of_computation_time + service_time
-        departure_times.append(departure_time)
-        previous_departure_time = departure_time
+        number_of_clients = sum(
+            1 for departure_time in departure_times if departure_time > arrival_time
+        )
+        if number_of_clients < buffer_size + 1:
+            beginning_of_computation_time = max(arrival_time, previous_departure_time)
+            departure_time = beginning_of_computation_time + service_time
+            departure_times.append(departure_time)
+            previous_departure_time = departure_time
+        else:
+            departure_times.append(-1)
     return departure_times
--- tests/_pyqueuesimu/generation/test_departure_times.py
+++ tests/_pyqueuesimu/generation/test_departure_times.py
@@ -3,7 +3,10 @@ import re
 
 import pytest
 
-from _pyqueuesimu.generation.departure_times import get_departure_times
+from _pyqueuesimu.generation.departure_times import (
+    get_departure_times,
+    get_departure_times_limited_buffer,
+)
 
 
 @pytest.mark.parametrize(
@@ -41,3 +44,50 @@ def test_get_departure_times__raise_when_size_differs(
 ) -> None:
     with pytest.raises(ValueError, match=re.escape(expected_error_message)):
         get_departure_times(arrival_times, service_times)
+
+
+@pytest.mark.parametrize(
+    ("arrival_times", "service_times", "buffer_size", "expected_result"),
+    [
+        ([1] * 4, [1] * 4, 10, [2, 3, 4, 5]),
+        ([1] * 4, [1] * 4, 1, [2, 3, -1, -1]),
+        ([1] * 4, [1] * 4, 2, [2, 3, 4, -1]),
+        ([0.1, 2.1, 3.8, 3.9], [1.1, 0.7, 2.1, 0.01], 1, [1.2, 2.8, 5.9, 5.91]),
+        ([0.1, 2.1, 3.8, 3.9], [1.1, 0.7, 2.1, 0.01], 0, [1.2, 2.8, 5.9, -1]),
+        ([0.1, 1, 3.8, 3.9, 6], [1.1, 0.7, 2.1, 0.01, 2], 0, [1.2, -1, 5.9, -1, 8]),
+        ([1, 2, 3, 4, 5, 6], [5, 4, 5, 7, 1, 2], 2, [6, 10, 15, -1, -1, 17]),
+        ([], [], 9, []),
+    ],
+)
+def test_get_departure_times_limited_buffer(
+    *,
+    arrival_times: list[float],
+    service_times: list[float],
+    buffer_size: int,
+    expected_result: list[float],
+) -> None:
+    result = get_departure_times_limited_buffer(
+        arrival_times, service_times, buffer_size
+    )
+    print(expected_result)
+    print(result)
+    assert all(
+        math.isclose(elem_result, elem_expected_result)
+        for elem_result, elem_expected_result in zip(
+            result, expected_result, strict=True
+        )
+    )
+
+
+@pytest.mark.parametrize(
+    ("arrival_times", "service_times", "expected_error_message"),
+    [
+        ([], [1], "len(arrival_times) != len(service_times): 0 != 1"),
+        ([5, 2, 7], [1], "len(arrival_times) != len(service_times): 3 != 1"),
+    ],
+)
+def test_get_departure_times_limited_buffer__raise_when_size_differs(
+    arrival_times: list[float], service_times: list[float], expected_error_message: str
+) -> None:
+    with pytest.raises(ValueError, match=re.escape(expected_error_message)):
+        get_departure_times_limited_buffer(arrival_times, service_times, 100)
